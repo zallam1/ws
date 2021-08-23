030143755fc21c32f1c7a269bfd170490c4ce722
Zakia ALLAM <htc.zall21@gmail.com>
2021-08-23 17:56:45 +0200

    add simple project

---
+++ helloworld/Projet1/src/sdz1.java
@@ -0,0 +1,11 @@
+
+public class sdz1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world !!");
+		syso
+	}
+
+}
+ 
---
+++ helloworld/test-jar/src/fr/htc/test/Runit.java
@@ -0,0 +1,21 @@
+package fr.htc.test;
+
+
+public class Runit {
+
+	public static void main(String[] args) {
+
+		List<String> arguments = new ArrayList<>();
+		for (String arg : args) {
+			if (arg.length() > 3) {
+				arguments.add(arg);
+			}
+		}
+
+		System.out.println("LE nombre d'arguments de taille > à 3 est " + arguments.size());
+		System.out.println("Voici la list :");
+		for (String argSup3 : arguments) {
+			System.out.println(argSup3);
+		}
+	}
+}
